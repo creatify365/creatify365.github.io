c4263e20dc272feec9b502216793cc6ab9967013
Gerald Patrick <creatify365@gmail.com>
2025-04-04 15:31:12 +0300

    Updated package.json for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  // Add this to ensure assets load correctly
+  assetPrefix: process.env.NODE_ENV === "production" ? "/" : "",
 };
 
 export default nextConfig;
